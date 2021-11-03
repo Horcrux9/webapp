fcecbb47589faf9b35046314ca22c22efd964740
Maharshi Jinandra <jinandra.m@northeastern.edu>
2021-11-03 00:02:17 -0400

    codedeploy api changes 5

--- codedeploy/applicationStop.sh
+++ codedeploy/applicationStop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "process id"
-PID=`ps -eaf | grep "node app.js" | grep -v grep | awk '{print $2}'`
+PID=`ps aux | grep "node app.js" | grep -v grep | awk '{print $2}'`
 echo "process id not empty ? $PID"
 if [[ "" !=  "$PID" ]]; then
   echo "killing $PID"
