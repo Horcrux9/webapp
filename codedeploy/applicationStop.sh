459f9f044983e2c1a1445ab7db865801eb9a38df
Maharshi Jinandra <jinandra.m@northeastern.edu>
2021-11-04 14:38:57 -0400

    codedeploy script changes 14

--- codedeploy/applicationStop.sh
+++ codedeploy/applicationStop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+#!/bin/bash
+#Stopping existing node servers
+echo "Stopping any existing node servers"
+pkill node
