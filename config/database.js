63607dcb1e26be8d8acfa46da0f3459ae22de990
Maharshi Jinandra <jinandra.m@northeastern.edu>
2021-11-28 23:32:36 -0500

    init read replica settings

--- config/database.js
+++ config/database.js
@@ -1,6 +1,22 @@
-const { Sequelize } = require('sequelize');
- 
-const { MODE } = require("./env");
-const config = require("./config.js")[MODE];
+const {
+    Sequelize
+} = require('sequelize');
 
-module.exports = new Sequelize(config.database, config.username, config.password, config);
+const {
+    MODE
+} = require("./env");
+const config = require("./config.js")[MODE];
+const readReplica = require("./config.js")["read-replica"];
+
+// module.exports = new Sequelize(config.database, config.username, config.password, config);
+module.exports = new Sequelize("database", null, null, {
+    dialect: "postgres",
+    replication: {
+        write: {
+            ...config
+        },
+        read: {
+            ...readReplica
+        }
+    }
+});
