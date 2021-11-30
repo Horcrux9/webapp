e3ab55f798333df90e27059a9d34355b324addce
Maharshi Jinandra <jinandra.m@northeastern.edu>
2021-11-30 12:24:58 -0500

    read only fields updated to have verified & verified_on

--- v2/user/update.js
+++ v2/user/update.js
@@ -1,19 +1,39 @@
-const { encryptPss, validateEmail, passwordCheck } = require(__dirname + "./../../utils/user_utils");
-const { end_time_post } = require(__dirname + "./../../utils/statsd_utils");
+const {
+    encryptPss,
+    validateEmail,
+    passwordCheck
+} = require(__dirname + "./../../utils/user_utils");
+const {
+    end_time_post
+} = require(__dirname + "./../../utils/statsd_utils");
 const statsd_client = require(__dirname + "./../../utils/statsd");
+
+const readonlyfields = [
+    "id",
+    "pk",
+    "account_created",
+    "account_updated",
+    "verified",
+    "verified_on",
+];
 
 const update = async (user, payload) => {
     /* console.log("HERE ", req); */
 
     if (payload && Object.keys(payload).length == 0) {
-        return { status: 400 };
+        return {
+            status: 400
+        };
     }
 
     /**
      * check for read only fields
      */
-    if ("id" in payload || "pk" in payload || "account_created" in payload || "account_updated" in payload) {
-        return { status: 400, message: "ReadOnly field provided" };
+    if (readonlyfields.filter(f => Object.keys(payload).includes(f)).length != 0) {
+        return {
+            status: 400,
+            message: "ReadOnly field provided"
+        };
     }
 
     if ("first_name" in payload && payload.first_name == '') {
@@ -25,7 +45,7 @@ const update = async (user, payload) => {
 
     if ("password" in payload) {
         const passCheck = passwordCheck(payload.password);
-        if(passCheck.status != 200) {
+        if (passCheck.status != 200) {
             return passCheck;
         }
         payload.password = await encryptPss(payload.password);
@@ -33,19 +53,27 @@ const update = async (user, payload) => {
 
     if ("username" in payload) {
         const unameCheck = validateEmail(payload.username);
-        if(unameCheck.status != 200) {
+        if (unameCheck.status != 200) {
             return unameCheck;
         }
     }
 
     try {
         const start_time = new Date();
-        const ans = await user.update({ ...payload, account_updated: Date.now() });
+        const ans = await user.update({
+            ...payload,
+            account_updated: Date.now()
+        });
         // cloudwatch metric
         statsd_client.timing(`UPDATE-QUERY`, (new Date() - start_time));
-        return { status: 204 };
+        return {
+            status: 204
+        };
     } catch (error) {
-        return { status: 400, message: error.message };
+        return {
+            status: 400,
+            message: error.message
+        };
     }
 
 };
@@ -54,9 +82,14 @@ module.exports = ('/', async (req, res) => {
     try {
         const response = await update(req.user, req.body);
         end_time_post(req);
-        return res.status(response.status).json({ ...response, status: undefined });
+        return res.status(response.status).json({
+            ...response,
+            status: undefined
+        });
     } catch (error) {
         end_time_post(req);
-        return res.status(400).json({ message: error.message });
+        return res.status(400).json({
+            message: error.message
+        });
     };
 });
