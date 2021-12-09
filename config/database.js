198fecd149c994d0cc425f27992bbe9170af95ed
Maharshi Jinandra <jinandra.m@northeastern.edu>
2021-12-09 11:59:06 -0500

    init A08

--- config/database.js
+++ config/database.js
@@ -11,6 +11,9 @@ const readReplica = require("./config.js")["read-replica"];
 // module.exports = new Sequelize(config.database, config.username, config.password, config);
 module.exports = new Sequelize(config.database, config.username, config.password, {
     dialect: "postgres",
+    dialectOptions: {
+       ssl: true
+    },
     replication: {
         read: {
             host: readReplica.host,
